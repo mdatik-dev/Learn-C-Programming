4fac33acb2d4ef8d54cd31257d4f960b1fecdc48
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-05 16:12:19 +0600

    Add files via upload

---
+++ 11) Arrays in C/1D Arrays in C/Write a C program to find the maximum and minimum number in an array..c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num, max, min;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &num);
+    int arr[num];
+
+    printf("Enter %d elements:\n", num);
+    for (int i = 0; i < num; i++) {
+        scanf("%d", &arr[i]);
+    }
+    max = arr[0];
+    min = arr[0];
+
+    for (int i = 1; i < num; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    printf("Maximum value = %d\n", max);
+    printf("Minimum value = %d\n", min);
+
+    return 0;
+}
+
