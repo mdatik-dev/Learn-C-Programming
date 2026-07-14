d331e80904513a6cf34397dde30aadfd6df83b5b
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-15 01:00:12 +0600

    Add files via upload

---
+++ 6) IF Else in C/Check Digit or Alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if(ch >= '0' && ch <= '9')
+        printf("Digit");
+    else if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        printf("Alphabet");
+    else
+        printf("Special Character");
+
+    return 0;
+}
