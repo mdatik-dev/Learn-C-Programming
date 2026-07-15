9f3f996e49262650db0778130041401fa84dd870
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-16 00:05:01 +0600

    Add files via upload

---
+++ 6) IF Else in C/Check Divisible by 2 and 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0 && num % 3 == 0)
+        printf("Divisible by both 2 and 3");
+    else
+        printf("Not Divisible by both 2 and 3");
+
+    return 0;
+}
