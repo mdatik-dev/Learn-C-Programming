d78a40a3edf4933b41e01c90c90e8ff6bc513ced
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-17 01:39:11 +0600

    Add files via upload

---
+++ 6) IF Else in C/Check Driving License Eligibility.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if(age >= 18)
+        printf("Eligible for Driving License");
+    else
+        printf("Not Eligible for Driving License");
+
+    return 0;
+}
