076d4e9e49c5ab420bb46188d4ef8788c965ef7f
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-24 00:11:23 +0600

    Add files via upload

---
+++ 6) IF Else in C/Check Uppercase or Lowercase.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if(ch >= 'A' && ch <= 'Z')
+        printf("Uppercase Letter");
+    else if(ch >= 'a' && ch <= 'z')
+        printf("Lowercase Letter");
+    else
+        printf("Not an Alphabet");
+
+    return 0;
+}
