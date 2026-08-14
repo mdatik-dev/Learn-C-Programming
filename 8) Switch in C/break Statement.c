f823792a9ab1773885d408b68ac2ad39dd2d7736
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-08-15 02:57:55 +0600

    Add files via upload

---
+++ 8) Switch in C/break Statement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int n = 2;
+
+    switch(n)
+    {
+        case 1:
+            printf("One\n");
+
+        case 2:
+            printf("Two\n");
+
+        case 3:
+            printf("Three\n");
+    }
+
+    return 0;
+}
