d2ec5deb97615d9f0ddccab9cd9adbe9dfbab1db
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-08-18 22:40:29 +0600

    Add files via upload

---
+++ 8) Switch in C/Problem 12 Student Grade Description.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    char grade;
+
+    printf("Enter Grade: ");
+    scanf(" %c", &grade);
+
+    switch(grade) {
+        case 'A':
+            printf("Excellent");
+            break;
+        case 'B':
+            printf("Very Good");
+            break;
+        case 'C':
+            printf("Good");
+            break;
+        case 'D':
+            printf("Average");
+            break;
+        case 'F':
+            printf("Fail");
+            break;
+        default:
+            printf("Invalid Grade");
+    }
+
+    return 0;
+}
