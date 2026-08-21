192d65d3b055e44d9061849923623e61001a8c5d
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-08-22 02:32:10 +0600

    Add files via upload

---
+++ 9) Loops in C/Problem 9 Sum of Odd Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i, sum = 0;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i += 2) {
+        sum += i;
+    }
+
+    printf("Odd Sum = %d", sum);
+
+    return 0;
+}
