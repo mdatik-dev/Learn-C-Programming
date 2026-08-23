fe0ab3180049cba1e0cc092ef4a0878e3645250c
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-08-23 23:21:52 +0600

    Add files via upload

---
+++ 9) Loops in C/Problem 13 Reverse Multiplication Table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i;
+
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+
+    for(i = 10; i >= 1; i--) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
