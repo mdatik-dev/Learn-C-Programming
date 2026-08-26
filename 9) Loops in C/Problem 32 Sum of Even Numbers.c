964eb81d164d5934152e080f6e81239ae0d6d0a6
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-08-27 03:03:21 +0600

    Add files via upload

---
+++ 9) Loops in C/Problem 32 Sum of Even Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i, sum = 0;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i += 2) {
+        sum += i;
+    }
+
+    printf("Even Sum = %d", sum);
+
+    return 0;
+}
