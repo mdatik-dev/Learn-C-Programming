5a7e9c3276360aba97a3ca88e4bfd7ac3f193bed
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-09-01 23:12:05 +0600

    Add files via upload

---
+++ 9) Loops in C/Example 7 Factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i = 1;
+    long long fact = 1;
+
+    printf("Enter Number: ");
+    scanf("%d", &n);
+
+    while(i <= n) {
+        fact *= i;
+        i++;
+    }
+
+    printf("Factorial = %lld", fact);
+
+    return 0;
+}
