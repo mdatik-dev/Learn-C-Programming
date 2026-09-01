86ce61dda2d30b4894b8efb2673c8c2a826ae28d
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-09-01 23:10:44 +0600

    Add files via upload

---
+++ 9) Loops in C/Example 5 Sum of 1 to N.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i = 1, sum = 0;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    while(i <= n) {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
