977a361e42dced9da04cce3431154f5dd2dcb33b
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-09-05 03:12:00 +0600

    Add files via upload

---
+++ 9) Loops in C/Nested Loops in C/Problem 11 Matrix Subtraction.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int a[2][2] = {{8, 6}, {5, 4}};
+    int b[2][2] = {{3, 2}, {1, 4}};
+    int c[2][2];
+    int i, j;
+
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+
+    printf("Subtraction Matrix:\n");
+
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
