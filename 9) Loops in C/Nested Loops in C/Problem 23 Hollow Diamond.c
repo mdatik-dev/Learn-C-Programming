313bc68a91820630d0c8c33439b2536d291a6ce0
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-09-07 00:38:46 +0600

    Add files via upload

---
+++ 9) Loops in C/Nested Loops in C/Problem 23 Hollow Diamond.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+
+    int i, j, space;
+
+    for(i = 1; i <= 5; i++) {
+
+        for(space = 1; space <= 5 - i; space++)
+            printf(" ");
+
+        for(j = 1; j <= 2 * i - 1; j++) {
+
+            if(j == 1 || j == 2 * i - 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+
+    for(i = 4; i >= 1; i--) {
+
+        for(space = 1; space <= 5 - i; space++)
+            printf(" ");
+
+        for(j = 1; j <= 2 * i - 1; j++) {
+
+            if(j == 1 || j == 2 * i - 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
