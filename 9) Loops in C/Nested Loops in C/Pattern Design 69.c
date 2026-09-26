c7fa9b0c5662c7c88c785e29ddbbd3c673289f0d
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-09-27 03:17:48 +0600

    Add files via upload

---
+++ 9) Loops in C/Nested Loops in C/Pattern Design 69.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n, row, col;
+    printf("Enter n : ");
+    scanf("%d", &n);
+
+    for(row = 1; row<= n; row++) {
+        for(col = 1; col<= n; col++){
+            printf("%d ",64+row);
+        }
+    printf("\n");
+    }
+}
